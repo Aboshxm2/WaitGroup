9ab9d1a91ac7576b2ef8e813e11862d5e0b0bfab
Aboshxm2 <72500560+Aboshxm2@users.noreply.github.com>
2023-04-12 23:19:47 +0300

    Upload the virion

---
+++ src/Aboshxm2/WaitGroup/WaitGroup.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Aboshxm2\WaitGroup;
+
+use Closure;
+
+class WaitGroup
+{
+    private int $count = 0;
+    private Closure $then;
+
+    public function add(int $count): void
+    {
+        $this->count += $count;
+    }
+
+    public function done(): void
+    {
+        $this->count -= 1;
+
+        if($this->count <= 0) {
+            ($this->then)();
+        }
+    }
+
+    public function wait(Closure $then): void
+    {
+        $this->then = $then;
+    }
+}
